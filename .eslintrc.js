20f3f8b0ea665951c437f60e53493c7ad9276fe6
Javier Toledo <jtoledo@corporacionmastergroup.com>
2019-06-23 22:50:18 -0600

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  },
+  extends: [
+    '@nuxtjs'
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error',
+    "comma-dangle": ["error", {
+        "arrays": "always-multiline",
+        "objects": "always-multiline",
+        "imports": "always-multiline",
+        "exports": "always-multiline",
+        "functions": "never",
+    }]
+  }
+}
