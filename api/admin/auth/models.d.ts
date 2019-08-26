7aca328973111a2ef26deb60513b232ee002e621
Fernando Gomez <fgomez@masterdevel.com>
2019-08-26 15:19:22 +0000

    Merged in ft/general-config (pull request #2)
    implement support for ts spec files, apply fix to interface declaration error, implement fix to component decorator error when testing
    
    * implement support for ts spec files, apply fix to interface declaration error, implement fix to component decorator error when testing
    
    Approved-by: Fernando Navarijo
    Approved-by: Javier Toledo

--- api/admin/auth/models.d.ts
+++ api/admin/auth/models.d.ts
@@ -1,23 +1,23 @@
 /// <reference path="../../models.d.ts" />
 declare namespace Models {
-    export interface LoginParams {
-        username: string,
-        password: string,
-        totp?: string
-    }
+  export interface LoginParams {
+    username: string;
+    password: string;
+    totp?: string;
+  }
 
-    export interface LoginResponse {
-        success: boolean,
-        result: {
-            token?: boolean,
-            tfa_secret?: boolean,
-            reset_password?: boolean,
-            ask_tfa?: boolean,
-        }
-    }
+  export interface LoginResponse {
+    success: boolean;
+    result: {
+      token?: boolean;
+      tfa_secret?: boolean;
+      reset_password?: boolean;
+      ask_tfa?: boolean;
+    };
+  }
 
-    export interface AdminResponse = {
-        success: boolean,
-        result: any,
-    }    
+  export interface AdminResponse {
+    success: boolean;
+    result: any;
+  }
 }
