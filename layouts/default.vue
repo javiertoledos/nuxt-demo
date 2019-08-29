027ee51037ea177bde7607f4b1167fef1f28b267
Javier Toledo <jtoledo@corporacionmastergroup.com>
2019-08-29 17:06:04 -0600

    Add lang switcher

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,31 @@
 <template lang="pug">
   div
+    nav.navbar(role="navigation" aria-label="main-navigation")
+      .navbar-menu
+        .navbar-end
+            nuxt-link.navbar-item(
+              v-for="locale in availableLocales"
+              :key="locale.code"
+              :to="switchLocalePath(locale.code)"
+              :class="{ 'is-active': locale.code === currentLocale }"
+            )  {{ locale.code  }}
     .section
       .container
         nuxt
 </template>
+<script type="ts">
+import { Vue, Component, State } from 'nuxt-property-decorator';
 
+@Component
+export default class LoginPage extends Vue {
+  get availableLocales () {
+    return this.$i18n.locales
+  }
+  get currentLocale() {
+    return this.$i18n.locale
+  }
+}
+</script>
 <style lang="scss">
 html {
     background-color: 	#fafafa;
