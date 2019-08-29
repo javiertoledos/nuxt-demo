a74ed24208ee7eb886d5de8faa36412004174f79
Javier Toledo <jtoledo@corporacionmastergroup.com>
2019-08-29 16:46:26 -0600

    Cleanup homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,6 @@
-<template>
-  <div class="container">
-    <div>
-      <logo class="logo" />
-      <h1 class="title" v-t="'test.hello-world'" />
-      <h2 class="subtitle">
-        Nuxt demo managers spa
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
-    </div>
-  </div>
+<template lang="pug">
+  div
+    .h1.title {{ $t('home.hello-world-title')}}
 </template>
 
 <script>
@@ -27,6 +8,7 @@ import { Vue, Component } from 'vue-property-decorator';
 import Logo from '~/components/Logo.vue'
 
 @Component({
+  middleware: ["auth"],
   components: {
     Logo
   }  
@@ -37,39 +19,5 @@ export default class Index extends Vue {
 </script>
 
 <style lang="scss">
-@include mobile {
-  .logo {
-    display: none;
-  }
-}
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
