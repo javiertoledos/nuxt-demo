b170caf7e943b093dcd0b0801d17b83ab9658e60
Javier Toledo <jtoledo@corporacionmastergroup.com>
2019-08-29 16:45:26 -0600

    Add header to work with postman mock

--- plugins/initApi.ts
+++ plugins/initApi.ts
@@ -1,6 +1,8 @@
 import { Axios } from '@/api';
 
 export default function (context: any) {
+    context.$axios.defaults.headers.common['x-mock-match-request-headers']
+
     if (!context.$axios) {
         console.error('Configure Nuxt Axios plugin');
     } else {
